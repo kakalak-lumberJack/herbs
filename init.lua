fb5a7b4a345a786890210c3d5e5bb0dfd780e72e
tacotexmex <tacotexmex@minetest.land>
2017-06-18 20:19:50 +0200

    Replace deprecated settings method

--- init.lua
+++ init.lua
@@ -1,17 +1,17 @@
 dofile(minetest.get_modpath("herbs").."/playereffects.lua")
 
-if minetest.setting_getbool("herbs_shroom") ~= false then
+if minetest.settings:get_bool("herbs_shroom") ~= false then
   dofile(minetest.get_modpath("herbs").."/shroom.lua")
 end
 
-if minetest.setting_getbool("herbs_mortar") ~= false then
+if minetest.settings:get_bool("herbs_mortar") ~= false then
   dofile(minetest.get_modpath("herbs").."/mortar.lua")
 end
 
-if minetest.setting_getbool("herbs_tincures") ~= false then
+if minetest.settings:get_bool("herbs_tincures") ~= false then
   dofile(minetest.get_modpath("herbs").."/tinctures.lua")
 end
 
-if minetest.setting_getbool("herbs_chats") ~= false then
+if minetest.settings:get_bool("herbs_chats") ~= false then
   dofile(minetest.get_modpath("herbs").."/chats.lua")
 end
